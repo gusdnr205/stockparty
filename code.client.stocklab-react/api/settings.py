dae56c51eb03d0171af27eb7037ce7cb1df49559
gusdnr205 <37622685+gusdnr205@users.noreply.github.com>
2023-07-11 15:56:24 +0900

    Update settings.py

--- code.client.stocklab-react/api/settings.py
+++ code.client.stocklab-react/api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'csi3-!yz6g%^oz9lu#us)4k=ux!zh6c)@)igw7$li$j6bt4i7%'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
